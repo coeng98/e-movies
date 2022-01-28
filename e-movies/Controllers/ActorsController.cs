24087f1260653cb0d062221f1bc5ce0a7d23d73a
coeng98 <coeng98@users.noreply.github.com>
2022-01-28 21:12:14 +0300

    Proje dosyası ekle.

---
+++ e-movies/Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using e_movies.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_movies.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View(data);
+        }
+    }
+}
